212d51a92d856d70c4f14e3685ade753588dd1c6
Andrew Larsson <andrew@developersbliss.com>
2025-02-25 23:45:35 -0700

    Initial commit. Start of rearchitecting Circle of Trust using OLDMAP.

---
+++ AndrewLarsson.CircleOfTrust/CircleService.cs
@@ -0,0 +1,25 @@
+﻿namespace AndrewLarsson.CircleOfTrust;
+public class CircleService {
+	public (Circle, ClaimCircleEvent) ClaimCircle(
+		ClaimCircle message
+	) {
+		var (circle, @event) = Circle.Claim(message);
+		return (circle, @event);
+	}
+
+	public (Circle, JoinCircleEvent) JoinCircle(
+		Circle circle,
+		JoinCircle message
+	) {
+		var @event = circle.Join(message);
+		return (circle, @event);
+	}
+
+	public (Circle, BetrayCircleEvent) BetrayCircle(
+		Circle circle,
+		BetrayCircle message
+	) {
+		var @event = circle.Betray(message);
+		return (circle, @event);
+	}
+}
---
+++ AndrewLarsson.CircleOfTrust/Program.cs
@@ -0,0 +1,15 @@
+﻿using AndrewLarsson.CircleOfTrust;
+using developersBliss.OLDMAP.Hosting;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder applicationBuilder = Host.CreateApplicationBuilder();
+applicationBuilder.Services
+	.AddOLDMAP()
+	.AddKafkaDomainMessageApplication()
+	.AddKafkaDomainEventApplication()
+	.AddPostgreSqlMartenAggregateRootStorage()
+	.TryAddPostgreSqlMartenAggregateRootStore<Circle>()
+	.AddApplicationServiceWithPureStyle<CircleService, Circle>()
+;
+IHost host = applicationBuilder.Build();
+await host.RunAsync();
