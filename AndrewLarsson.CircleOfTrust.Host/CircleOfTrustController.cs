d93aab6663d2bd66405d0fb58e147b9b06823e66
Andrew Larsson <andrew@developersbliss.com>
2025-03-12 00:57:20 -0600

    Use new IDomainRequester interface.

--- AndrewLarsson.CircleOfTrust.Host/CircleOfTrustController.cs
+++ AndrewLarsson.CircleOfTrust.Host/CircleOfTrustController.cs
@@ -8,9 +8,9 @@ namespace AndrewLarsson.CircleOfTrust.Host;
 [Route("api/circle-of-trust")]
 public class CircleOfTrustController(IDomainRequester domainRequester, DomainMessagePacker domainMessagePacker) : ControllerBase {
 	[HttpPost("test-with-response")]
-	public async Task<object> TestWithResponse(string requestId) {
+	public async Task<PackedDomainEvent> TestWithResponse(string requestId) {
 		var user = Guid.NewGuid().ToString("N")[..5];
-		object response = await domainRequester.Request(domainMessagePacker.PackMessage(
+		PackedDomainEvent response = await domainRequester.Request(domainMessagePacker.PackMessage(
 			domainMessageId: requestId,
 			address: CircleAddress.For(user),
 			domainMessage: new ClaimCircle($"{user} Test Circle", user, "April Fools!")
